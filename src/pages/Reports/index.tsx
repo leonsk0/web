70436755b46cf69b5d1769dc43e07fccf0e1f19f
Mikael Vital <mikaelmrvital@gmail.com>
2023-05-30 02:10:36 -0300

    card de report criado

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -24,26 +24,53 @@ export default function Report() {
           </div>
 
           <div className="containerTextDescription">
-            <p>O pedido foi realizado pelo responsável Cleiton da Guerra, pois o garoto quer assistir o neymar ganhar o hexa.</p>
+            <p className="containerTextP">O pedido foi realizado pelo responsável Cleiton da Guerra, pois o garoto quer assistir o neymar ganhar o hexa.</p>
           </div>
 
           <div className="containerListCart">
             <div className="containerCart">
-              <span>Dia</span>
-              <p>Segunda</p>
-              <span>Horário</span>
-              <p>-</p>
+              <span className="spanCart">Dia</span>
+              <p className="pCart">Segunda</p>
+              <span className="spanCart">Horário</span>
+              <p className="pCart">-</p>
             </div>
 
             <div className="containerCart">
-              <span>Dia</span>
-              <p>Segunda</p>
-              <span>Horário</span>
-              <p>-</p>
+              <span className="spanCart">Dia</span>
+              <p className="pCart">Terça</p>
+              <span className="spanCart">Horário</span>
+              <p className="pCart">-</p>
             </div>
 
+            <div className="containerCart">
+              <span className="spanCart">Dia</span>
+              <p className="pCart">Quarta</p>
+              <span className="spanCart">Horário</span>
+              <p className="pCart">-</p>
+            </div>
+
+            <div id="containerCart" className="containerCart">
+              <span id="spanCart" className="spanCart">Dia</span>
+              <p id="pCart" className="pCart">Quinta</p>
+              <span id="spanCart" className="spanCart">Horário</span>
+              <p id="pCart" className="pCart">8h - 18h</p>
+            </div>
+
+            <div className="containerCart">
+              <span className="spanCart">Dia</span>
+              <p className="pCart">Sexta</p>
+              <span className="spanCart">Horário</span>
+              <p className="pCart">-</p>
+            </div>
           </div>
 
+          <footer>
+            <div>
+            <span>Data da solicitação:</span>
+            <span>08/12/2022</span>
+            </div>
+            <Link to='/confirmation'>Enviar relatório</Link>
+          </footer>
       </div>
       </div>
     </div>
